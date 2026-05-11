db3117ac216559bd6527e7bb1feecce7ec8a9786
wileykieran05 <wiley.kieran05@gmail.com>
2026-05-11 16:44:49 -0700

    Added initial index page

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const supabaseUrl = "https://YOUR-PROJECT.supabase.co";
-const supabaseAnonKey = "YOUR_ANON_PUBLIC_KEY";
+const supabaseUrl = "https://ornxlspufzmvapdrwexc.supabase.co";
+const supabaseAnonKey = "sb_publishable_09DuqSFb7R6isk9CTq_O8g_er5TUkYt";
 
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseAnonKey);
