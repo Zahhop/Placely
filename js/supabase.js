4fdd76f17aae958ab6980cc673ebd5558c9ea905
wileykieran05 <wiley.kieran05@gmail.com>
2026-05-11 17:08:45 -0700

    Added initial index page

--- js/supabase.js
+++
@@ -1,4 +0,0 @@
-const supabaseUrl = "https://ornxlspufzmvapdrwexc.supabase.co";
-const supabaseAnonKey = "sb_publishable_09DuqSFb7R6isk9CTq_O8g_er5TUkYt";
-
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseAnonKey);
